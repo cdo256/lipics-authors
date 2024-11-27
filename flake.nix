fb6811df64e876c834ce306c6e8dac9f0be8b054
Christina O'Donnell <cdo@mutix.org>
2024-11-27 11:12:53 +0000

    Change texmf/ to texmf-cdo/ to avoid collision.

--- flake.nix
+++ flake.nix
@@ -36,9 +36,12 @@
           '';
 
           installPhase = ''
-            mkdir -p $out/share/texmf/tex/latex/lipics/
+            # Use /share/texmf-cdo to avoid collsion.
+            # Really I only need tex output, but that requires a lot of overloading.
+            # See https://github.com/NixOS/nixpkgs/issues/16182.
+            mkdir -p $out/share/texmf-cdo/tex/latex/lipics/
             mkdir -p $tex/tex/latex/lipics/
-            cp *.pdf *.cls *.sty $out/share/texmf/tex/latex/lipics/
+            cp *.pdf *.cls *.sty $out/share/texmf-cdo/tex/latex/lipics/
             cp *.pdf *.cls *.sty $tex/tex/latex/lipics/
           '';
         };
